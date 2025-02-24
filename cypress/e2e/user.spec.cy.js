d696fcacc6cea0a121603972194adf6618a3d0b5
unknown <QUALIDADE@DESKTOP-1302MD8.solsis.br>
2025-02-24 12:30:56 -0300

    Scenario de edição e add de User

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -12,7 +12,10 @@ const selectorList = {
   myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
   firstNameField: "[name='firstName']",
   lastNameField: "[name='lastName']",
-  idNameField: ".oxd-input--active"
+  genericField: ".oxd-input--active",
+  dateField: "[placeholder='yyyy-mm-dd']",
+  dateCloseButton: ".--close",
+  saveButton: "[type='submit']"
   
 }
 
@@ -26,7 +29,17 @@ const selectorList = {
     cy.get(selectorList.myInfoButton).click()
     cy.get(selectorList.firstNameField).clear().type('firstNameTest')
     cy.get(selectorList.lastNameField).clear().type('lastNameTest')
-    cy.get(selectorList.idNameField).eq(3).clear().type('emploTest')
+    cy.get(selectorList.genericField).eq(3).clear().type('emploTest')
+    cy.get(selectorList.genericField).eq(4).clear().type('OherIdTest')
+    cy.get(selectorList.genericField).eq(5).clear().type('DriverLicenseTest')
+    cy.get(selectorList.genericField).eq(6).clear().type('2025-03-10')
+    cy.get(selectorList.dateCloseButton).click()
+    cy.get(selectorList.genericField).eq(7).clear().type('1999-07-24')
+    cy.get(selectorList.genericField).eq(8).clear().type('sinNumberTest')
+    //cy.get(selectorList.dateField)
+    cy.get(selectorList.saveButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+    cy.get('.oxd-toast-close').click()
 
   })
 
